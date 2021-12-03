8c772f4f8f5269287ef1823db20bd3ddd3404345
Hugues ALEXANDRE <hugues.alex@gmail.com>
2021-12-03 16:18:45 +0900

    day-3

---
+++ bolbovic/day3-1.js
@@ -0,0 +1,23 @@
+const inputToArray = require("./inputToArray");
+
+async function processing() {
+  let lines = 0;
+  let numberOfOnes = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+  (await inputToArray("input-day3.txt", (line) => line.split(""))).forEach(
+    (row) => {
+      row.forEach((bit, idx) => {
+        if (bit === "1") numberOfOnes[idx]++;
+      });
+      lines++;
+    }
+  );
+  const gamma = numberOfOnes
+    .map((number) => (number > lines / 2 ? 1 : 0))
+    .join("");
+  const epsilon = numberOfOnes
+    .map((number) => (number > lines / 2 ? 0 : 1))
+    .join("");
+  console.log(gamma, epsilon, parseInt(gamma, 2) * parseInt(epsilon, 2));
+}
+
+processing();
---
+++ bolbovic/day3-2.js
@@ -0,0 +1,37 @@
+const inputToArray = require("./inputToArray");
+
+async function processing() {
+  let oxArray = await inputToArray("input-day3.txt", (line) => {
+    const obj = {};
+    line.split("").forEach((bit, idx) => (obj[idx] = bit));
+    return obj;
+  });
+  let co2Array = JSON.parse(JSON.stringify(oxArray));
+  for (let i = 0; oxArray.length > 1; i++) {
+    const numberOfOnes = oxArray.reduce(
+      (tot, obj) => tot + parseInt(obj[i]),
+      0
+    );
+    const biggerNumber = numberOfOnes >= oxArray.length / 2 ? "1" : "0";
+    oxArray = oxArray.filter((obj) => obj[i] === biggerNumber);
+  }
+
+  for (let i = 0; co2Array.length > 1; i++) {
+    const numberOfOnes = co2Array.reduce(
+      (tot, obj) => tot + parseInt(obj[i]),
+      0
+    );
+    const biggerNumber = numberOfOnes >= co2Array.length / 2 ? "0" : "1";
+    co2Array = co2Array.filter((obj) => obj[i] === biggerNumber);
+  }
+
+  const oxygenGenerator = Object.values(oxArray[0]).join("");
+  const co2Scrubber = Object.values(co2Array[0]).join("");
+  console.log(
+    oxygenGenerator,
+    co2Scrubber,
+    parseInt(oxygenGenerator, 2) * parseInt(co2Scrubber, 2)
+  );
+}
+
+processing();
