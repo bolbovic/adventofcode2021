c1c4167e76528cfd9548de527dea40523f3e74f7
Hugues ALEXANDRE <hugues.alex@gmail.com>
2021-12-07 18:26:13 +0900

    day 7

---
+++ 7/1.js
@@ -0,0 +1,21 @@
+const inputToArray = require("../utils/inputToArray");
+
+async function processing() {
+  let crabs = (
+    await inputToArray((line) => line.split(",").map((num) => parseInt(num)))
+  )[0];
+
+  const max = crabs.reduce((max, v) => (v > max ? v : max));
+  const fuel = [];
+  for (let i = 0; i < max + 1; i++) {
+    fuel.push(crabs.reduce((tot, crab) => tot + Math.abs(crab - i), 0));
+  }
+  console.log(
+    fuel.reduce((min, v, idx) => (v < min.v ? { idx, v } : min), {
+      idx: 0,
+      v: 1000000000,
+    })
+  );
+}
+
+processing();
